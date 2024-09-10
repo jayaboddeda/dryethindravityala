79f700bceb1d87fd63d9e36dde0afa76fd55c35c
Rahul Katari <rahulkatari99@gmail.com>
2024-09-10 14:28:37 +0530

    added navbar from a js file.

---
+++ assets/js/navbar.js
@@ -0,0 +1,232 @@
+const navHtml = `<div class="preloader ">
+        <button class="th-btn preloaderCls d-none">Cancel Preloader </button>
+        <div class="preloader-inner">
+            <div class="loader"></div>
+        </div>
+    </div><!--==============================
+    Sidemenu
+============================== -->
+    <div class="sidemenu-wrapper sidemenu-cart ">
+        <div class="sidemenu-content">
+            <button class="closeButton sideMenuCls"><i class="far fa-times"></i></button>
+            <div class="widget woocommerce widget_shopping_cart d-none">
+                <h3 class="widget_title">Shopping cart</h3>
+                <div class="widget_shopping_cart_content">
+                    <ul class="woocommerce-mini-cart cart_list product_list_widget ">
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_thumb_1_1.jpg" alt="Cart Image">Surgery Hands Gloves</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>940.00</span>
+                            </span>
+                        </li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_thumb_1_2.jpg" alt="Cart Image">D-Ribose Drug Medicine</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>899.00</span>
+                            </span>
+                        </li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_thumb_1_3.jpg" alt="Cart Image">CAD Dentistry Prosthesis</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>756.00</span>
+                            </span>
+                        </li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_thumb_1_4.jpg" alt="Cart Image">Surgery Safety Mask</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>723.00</span>
+                            </span>
+                        </li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_thumb_1_5.jpg" alt="Cart Image">Infrared Thermometers</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>1080.00</span>
+                            </span>
+                        </li>
+                    </ul>
+                    <p class="woocommerce-mini-cart__total total">
+                        <strong>Subtotal:</strong>
+                        <span class="woocommerce-Price-amount amount">
+                            <span class="woocommerce-Price-currencySymbol">$</span>4398.00</span>
+                    </p>
+                    <p class="woocommerce-mini-cart__buttons buttons">
+                        <a href="cart.html" class="th-btn wc-forward">View cart</a>
+                        <a href="checkout.html" class="th-btn checkout wc-forward">Checkout</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div><!--==============================
+    Sidemenu
+============================== -->
+    <div class="sidemenu-wrapper sidemenu-info ">
+        <div class="sidemenu-content">
+            <button class="closeButton sideMenuCls"><i class="far fa-times"></i></button>
+            <div class="widget  ">
+                <div class="th-widget-about">
+                    <div class="about-logo">
+                        <a href="/"><img src="assets/img/logo.svg" alt="Mediax"></a>
+                    </div>
+                    <p class="about-text">Subscribe to out newsletter today to receive latest news administrate cost effective for tactical data.</p>
+                    <p class="footer-info">
+                        <i class="fal fa-location-dot"></i>
+                        2478 Street City Ohio 90255
+                    </p>
+                    <p class="footer-info">
+                        <i class="fal fa-envelope"></i>
+                        <a href="mailto:info@mediax.com" class="info-box_link">info@mediax.com</a>
+                    </p>
+                    <p class="footer-info">
+                        <i class="fal fa-phone"></i>
+                        <a href="tel:+40276328246" class="info-box_link">+(402) 763 282 46</a>
+                    </p>
+                    <div class="th-social">
+                        <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.whatsapp.com/"><i class="fab fa-whatsapp"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="widget  ">
+                <h3 class="widget_title">Let’s Stay In Touch</h3>
+                <div class="newsletter-widget">
+                    <p class="footer-text">Subscribe for newsletter</p>
+                    <form action="#" class="newsletter-form">
+                        <div class="form-group">
+                            <input class="form-control" type="email" placeholder="Enter Email" required="">
+                        </div>
+                        <button type="submit" class="simple-icon"><i class="fa-solid fa-paper-plane"></i></button>
+                    </form>
+                    <div class="form-group">
+                        <input type="checkbox" id="checkboxagree" name="checkboxagree">
+                        <label for="checkboxagree">I agree with the terms & conditions</label>
+                    </div>
+                    <div class="btn-group">
+                        <a href="https://play.google.com/" class="img-btn">
+                            <img src="assets/img/normal/apple_store.png" alt="icon">
+                        </a>
+                        <a href="https://play.google.com/" class="img-btn">
+                            <img src="assets/img/normal/play_store.png" alt="icon">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="popup-search-box d-none d-lg-block">
+        <button class="searchClose"><i class="fal fa-times"></i></button>
+        <form action="#">
+            <input type="text" placeholder="What are you looking for?">
+            <button type="submit"><i class="fal fa-search"></i></button>
+        </form>
+    </div><!--==============================
+    Mobile Menu
+  ============================== -->
+  <div class="th-menu-wrapper">
+    <div class="th-menu-area text-center">
+        <button class="th-menu-toggle"><i class="fal fa-times"></i></button>
+        <div class="mobile-logo">
+            <a href="/"><img src="assets/img/logo.png" alt="Mediax"></a>
+        </div>
+        <div class="th-mobile-menu">
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li><a href="about.html">About</a></li>
+                <li>
+                    <a href="publications.html">Publications</a>
+                </li>
+                <li>
+                    <a href="awards.html">Awards</a>
+                </li>
+                <li>
+                    <a href="blog.html">Blogs</a>
+                </li>
+                <li>
+                    <a href="news.html">News</a>
+                </li>
+                <li>
+                    <a href="lectures.html">Lectures</a>
+                </li>
+                <li>
+                    <a href="campaigns.html">Campaigns</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div><!--==============================
+	Header Area
+==============================-->
+    <header class="th-header header-layout1 header-layout5">
+        <div class="sticky-wrapper">
+            <!-- Main Menu Area -->
+            <div class="menu-area">
+                <div class="container">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-auto">
+                            <div class="header-logo align-content-center">
+                                <a href="/"><img src="assets/img/logo.png" width="250" height="100" alt="Dr. Yethindra Vityala."></a>
+                            </div>
+                        </div>
+                        <div class="col-auto d-none d-lg-inline-block">
+                            <nav class="main-menu d-none d-lg-inline-block">
+                                <ul>
+                                    <li>
+                                        <a href="/">Home</a>
+                                    </li>
+                                    <li><a href="about.html">About</a></li>
+                                    <li>
+                                        <a href="publications.html">Publications</a>
+                                    </li>
+                                    <li>
+                                        <a href="awards.html">Awards</a>
+                                    </li>
+                                    <li>
+                                        <a href="blog.html">Blogs</a>
+                                    </li>
+                                    <li>
+                                        <a href="news.html">News</a>
+                                    </li>
+                                    <li>
+                                        <a href="lectures.html">Lectures</a>
+                                    </li>
+                                    <li>
+                                        <a href="campaigns.html">Campaigns</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-auto ">
+                            <div class="header-button">
+                                <button type="button" class="d-none icon-btn searchBoxToggler"><i class="far fa-search"></i></button>
+                                <button type="button" class="icon-btn sideMenuCart d-none">
+                                    <span class="badge">5</span>
+                                    <i class="far fa-cart-shopping"></i>
+                                </button>
+                                <button type="button" class="icon-btn sideMenuInfo d-none"><i class="far fa-bars"></i></button>
+                                <a href="appointment.html" class="th-btn">Book Appointment  </a>
+                                <button type="button" class="th-menu-toggle d-block d-lg-none"><i class="far fa-bars"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>`;
+
+$(window).on('load', function () {
+    $('#navbar').html(navHtml);
+
+})
